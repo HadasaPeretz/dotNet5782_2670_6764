cd813f0f297a2af5ecc9da20b164f59e61ea4212
HadasaPeretz <dperez@g.jct.ac.il>
2021-10-10 15:00:50 +0300

    Add project files.

---
+++ Targil0/Program6764.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6764();
+            Welcome2670();
+            Console.ReadKey();
+        }
+        static partial void Welcome2670();
+        private static void Welcome6764()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0},welcom to my first console apliccation", username);
+            Console.ReadKey();
+        }
+    }
+}
